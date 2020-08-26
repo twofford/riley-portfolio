5a2477730c71050b897f60d5eaaa2092de236922
Taylor Wofford <taylor.wofford@gmail.com>
2020-08-26 16:47:19 -0400

    change home images

--- src/frontend/home.jsx
+++ src/frontend/home.jsx
@@ -1,87 +1,100 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
-import pancakes from '../images/6.jpg';
-import polenta from '../images/9.jpg';
-import cornedbeef from '../images/10.jpg';
-import chili from '../images/11.jpg';
-import eggbread from '../images/12.jpg';
-import noodles from '../images/17.jpg';
-import noodles2 from '../images/19.jpg';
-import pie from '../images/21.jpg';
-import eggrolls from '../images/5.jpeg';
-import tikka from '../images/8.jpeg';
-import margaritas from '../images/13.jpg';
-import cheesesticks from '../images/14.jpeg';
-import cheese from '../images/15.jpeg';
-import cheese2 from '../images/18.jpeg';
-import tartare from '../images/20.jpg';
+import lumpia1 from "../images/home/banana-lumpia-01-7002344_0.jpg";
+import lumpia2 from "../images/home/banana-lumpia-03-7002344.jpg";
+import pancakes1 from "../images/home/gluten-free-buttermilk-pancakes-01-7001990-1.jpg";
+import pancakes2 from "../images/home/gluten-free-buttermilk-pancakes-03-7001990_0.jpg";
+import chicken1 from "../images/home/instant-pot-chicken-thighs-7001428_horiz.jpg";
+import chicken2 from "../images/home/instant-pot-chicken-thighs-7001428_vert.jpg";
+import cornedbeef1 from "../images/home/instant-pot-corned-beef-01-7001990.jpg";
+import cornedbeef2 from "../images/home/instant-pot-corned-beef-03-7001990.jpg";
+import chili1 from "../images/home/instant-pot-turkey-chili-7001209-horiz.jpg";
+import chili2 from "../images/home/instant-pot-turkey-chili-7001209-vert.jpg";
+import khachapuri1 from '../images/home/khachapuri-04-7001708-horiz.jpg';
+import khachapuri2 from "../images/home/khachapuri-06-7001708-vert_0.jpg";
+import mezcal1 from "../images/home/lemon-mezcal-margarita-01-7003589.jpg";
+import mezcal2 from "../images/home/lemon-mezcal-margarita-02-7003589_0.jpg";
+import chowmein1 from "../images/home/shrimp-chow-mein-7000860-0518_horiz.jpg";
+import chowmein2 from "../images/home/shrimp-chow-mein-7000860-0518_vert.jpg";
+import blueberry1 from "../images/home/vegan-blueberry-crisp-B-7000860-0518_horiz.jpg";
+import blueberry2 from "../images/home/vegan-blueberry-crisp-B-7000860-0518_vert.jpg";
 
 const Home = () => {
   return (
     <>
       <Carousel className="d-none d-sm-block">
         <Carousel.Item>
-          <img className="w-100" src={pancakes} alt="TK" />
+          <img className="w-100" src={lumpia1} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={polenta} alt="TK" />
+          <img className="w-100" src={pancakes1} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={cornedbeef} alt="TK" />
+          <img className="w-100" src={chicken1} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={chili} alt="TK" />
+          <img className="w-100" src={cornedbeef1} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={eggbread} alt="TK" />
+          <img className="w-100" src={chili1} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={noodles} alt="TK" />
+          <img className="w-100" src={khachapuri1} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={noodles2} alt="TK" />
+          <img className="w-100" src={mezcal1} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={pie} alt="TK" />
+          <img className="w-100" src={chowmein1} alt="TK" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="w-100" src={blueberry1} alt="TK" />
         </Carousel.Item>
       </Carousel>
 
       <Carousel className="d-block d-sm-none">
         <Carousel.Item>
-          <img className="w-100" src={eggrolls} alt="TK" />
+          <img className="w-100" src={lumpia2} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={tikka} alt="TK" />
+          <img className="w-100" src={pancakes2} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={margaritas} alt="TK" />
+          <img className="w-100" src={chicken2} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={cheesesticks} alt="TK" />
+          <img className="w-100" src={cornedbeef2} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={cheese} alt="TK" />
+          <img className="w-100" src={chili2} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={cheese2} alt="TK" />
+          <img className="w-100" src={khachapuri2} alt="TK" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="w-100" src={tartare} alt="TK" />
+          <img className="w-100" src={mezcal2} alt="TK" />
         </Carousel.Item>
-        
+
+        <Carousel.Item>
+          <img className="w-100" src={chowmein2} alt="TK" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="w-100" src={blueberry2} alt="TK" />
+        </Carousel.Item>
+
       </Carousel>
     </>
   );
--- src/frontend/styling.jsx
+++ src/frontend/styling.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Styling = () => {
   return (
-    <div>Styling</div>
+    <Container>
+      <Row>
+        <Col>
+        </Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   );
 };
 
